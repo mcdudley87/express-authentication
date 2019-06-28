c6b5ceeadf9da628a9bbaf6230bb808713bef326
Michael <sound.plus.fury@gmail.com>
2019-06-28 12:11:51 -0700

    day 2 commit 1

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,12 @@ const db = require('../models');
 const passport = require('../config/passportConfig');
 const router = express.Router();
 
+
+//GET /auth/signup - sends the signup form
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
-
+//GET /auth/signup - receives data from that form above.
 router.post('/signup', function(req, res) {
   db.user.findOrCreate({
     where: {email: req.body.email},
@@ -18,30 +20,39 @@ router.post('/signup', function(req, res) {
     if (created) {
       console.log("User was create, not found.");
       passport.authenticate('local', {
-        successRedirect: '/'
+        successRedirect: '/',
+        successFlash: 'Account created and logged in!'
       })(req, res); //immediate invoked function expression, 'IIFE', "iffy".
     } else {
       console.log("Email already exists.");
+      req.flash('error', '❌❌Email already exists!❌❌');
       res.redirect('/auth/signup');
     } 
   }).catch(function(error) {
     console.log("Error:", error.message);
+    req.flash('error', error.message);
     res.redirect('/auth/signup');
   });
 });
 
+//GET /auth/login - sends the login form
 router.get('/login', function(req, res) {
   res.render('auth/login');
 });
+// POST /auth/login - does the athentification
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/auth/login'
+  failureRedirect: '/auth/login',
+  successFlash: 'You have logged in. Heh, log...',
+  failureFlash: 'Invalid username/password. Get it a wheelchair.'
 }));
 
-
+//GET /auth/logout - deletes the session
 router.get('/logout', function(req, res) {
   req.logout();
   console.log('logged out');
+  req.flash('success', 'You have logged out? Don\'t leave me...');
+  //attaching flash messages that will be sent on the next response.
   res.redirect('/');
 });
 
